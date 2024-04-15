7654b5f504ac18b963ba26370af2c58a98cc5eeb
nicelah <mzgao@uwaterloo.ca>
2024-04-15 11:04:22 +0800

    add animations and update logs continuosly

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const diskusage = require('diskusage');
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
-const logFilePath = "/root/.pm2/logs/my-app-out.log";
+const logFilePath = "/root/transcribemom/logs/translatemom.log";
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -17,18 +17,21 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
     console.log('A user connected');
 
-    // Send the last 50 lines of the log file to the connected client
-    fs.readFile(logFilePath, 'utf8', (err, data) => {
-        if (err) {
-            console.error(`Error reading log file: ${err}`);
-        } else {
-            const lines = data.split('\n').slice(-50); // Get the last 50 lines
-            socket.emit('log-update', lines.join('\n'));
-        }
-    });
+    // Refresh log every 2 seconds
+    const logInterval = setInterval(() => {
+        fs.readFile(logFilePath, 'utf8', (err, data) => {
+            if (err) {
+                console.error(`Error reading log file: ${err}`);
+                socket.emit('log-error', 'Failed to read log file');
+            } else {
+                const lines = data.split('\n').slice(-50); // Get the last 50 lines
+                socket.emit('log-update', lines.join('\n'));
+            }
+        });
+    }, 2000);
 
     // Send system info every 10 seconds
-    const interval = setInterval(async () => {
+    const systemInfoInterval = setInterval(async () => {
         try {
             const path = os.platform() === 'win32' ? 'c:' : '/';
             const { available, free, total } = await diskusage.check(path);
@@ -42,12 +45,14 @@ io.on('connection', (socket) => {
             socket.emit('system-info', systemInfo);
         } catch (err) {
             console.error('Failed to get disk usage:', err);
+            socket.emit('system-error', 'Failed to get system info');
         }
     }, 10000);
 
     socket.on('disconnect', () => {
         console.log('User disconnected');
-        clearInterval(interval);
+        clearInterval(logInterval);
+        clearInterval(systemInfoInterval);
     });
 });
 
