7a5d889abe5971c73c2283c122729826bbda7cad
nicelah <mzgao@uwaterloo.ca>
2024-04-15 11:40:43 +0800

    add a helluva logs

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const socketIo = require('socket.io');
 const fs = require('fs');
 const os = require('os');
 const diskusage = require('diskusage');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
-const logFilePath = "/root/transcribemom/logs/translatemom.log";
+
+const translatemomLogPath = "/root/transcribemom/logs/translatemom.log";
+const indexOutLogPath = path.join(os.homedir(), ".pm2/logs/index-out.log");
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -17,9 +20,8 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
     console.log('A user connected');
 
-    // Refresh log every 2 seconds
-    const logInterval = setInterval(() => {
-        fs.readFile(logFilePath, 'utf8', (err, data) => {
+    const sendLogs = () => {
+        fs.readFile(translatemomLogPath, 'utf8', (err, data) => {
             if (err) {
                 console.error(`Error reading log file: ${err}`);
                 socket.emit('log-error', 'Failed to read log file');
@@ -28,18 +30,37 @@ io.on('connection', (socket) => {
                 socket.emit('log-update', lines.join('\n'));
             }
         });
-    }, 2000);
+
+        fs.readFile(indexOutLogPath, 'utf8', (err, data) => {
+            if (err) {
+                console.error(`Error reading index-out log file: ${err}`);
+            } else {
+                const jsonPattern = /data: ({.*})/;
+                const match = data.match(jsonPattern);
+                if (match) {
+                    const jsonData = JSON.parse(match[1]);
+                    const elapsedTime = Date.now() - parseInt(jsonData.data.id);
+                    socket.emit('tweet-info', `Elapsed Time: ${Math.floor(elapsedTime / 1000)} seconds`);
+                }
+            }
+        });
+    };
+
+    // Initial send
+    sendLogs();
+
+    // Refresh log every 2 seconds
+    const logInterval = setInterval(sendLogs, 2000);
 
     // Send system info every 10 seconds
     const systemInfoInterval = setInterval(async () => {
         try {
-            const path = os.platform() === 'win32' ? 'c:' : '/';
-            const { available, free, total } = await diskusage.check(path);
+            const diskInfo = await diskusage.check(path.parse(os.platform() === 'win32' ? 'c:' : '/').root);
             const systemInfo = {
                 freemem: os.freemem(),
                 totalmem: os.totalmem(),
-                freeSpace: free,
-                totalSpace: total,
+                freeSpace: diskInfo.free,
+                totalSpace: diskInfo.total,
                 cpus: os.cpus().length
             };
             socket.emit('system-info', systemInfo);
